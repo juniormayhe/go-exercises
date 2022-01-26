6aab0fdad71a55098784fe695d56c873f82bf749
Junior Mayhé <juniormayhe@gmail.com>
2022-01-26 18:54:00 +0000

    slice int array in chunks

---
+++ slice-int-array/go.mod
@@ -0,0 +1,5 @@
+module exercises
+
+go 1.17
+
+require github.com/juniormayhe/currentFilename v0.0.0-20220125164439-f3e08984a506 // indirect
---
+++ slice-int-array/slice-int-array.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/juniormayhe/currentFilename"
+)
+
+func main() {
+	fmt.Println(currentFilename.GetCurrentFileName())
+
+	arr := []int{1, 2, 3, 4, 5}
+	printSlice("arr	| ", arr)
+
+	maxElements := 2
+	for i := 0; i < len(arr); i += maxElements {
+		max := min(i+maxElements, len(arr))
+		chunk := arr[i:max]
+		printSlice(fmt.Sprintf("arr[%v:%v]| ", i, max), chunk)
+	}
+
+	// since we didn't modify the array in the loop, the elements are the same
+	printSlice("arr	| ", arr)
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
+}
+
+func printSlice(m string, s []int) {
+	fmt.Printf("%v len=%d cap=%d address=%p value=%v\n", m, len(s), cap(s), s, s)
+}
