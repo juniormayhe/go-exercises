3d208f2510a42065329abedfd7f87d2c13453894
Junior Mayhé <juniormayhe@gmail.com>
2022-01-26 20:22:28 +0000

    reduce code

--- slice-make/slice-make.go
+++ slice-make/slice-make.go
@@ -14,19 +14,17 @@ func main() {
 	printSlice("s", s)
 
 	// we can append more items then capacity
-	t := append(s, 1)
-	t = append(t, 2)
-	t = append(t, 3)
-	t = append(t, 4)
+	t := append(s, 1, 2, 3, 4)
+
 	printSlice("t", t) // [0 0 0 1 2 3 4]
 
-	u := s[:5] // [0 0 0 1 2]
+	u := t[:5] // [0 0 0 1 2]
 	printSlice("u", u)
 
-	u = s[:3] // [0 0 0]
+	u = t[:3] // [0 0 0]
 	printSlice("u", u)
 
-	// u still points to underlying array s which has [0 0 0 1 2] so we can set low (3) and high limit (5).
+	// u still points to underlying array t which has [0 0 0 1 2] so we can set low (3) and high limit (5).
 	// a new array is created in a different memory address because we moved the low limit pointer
 	v := u[3:5] //  [1 2]
 	printSlice("v", v)
